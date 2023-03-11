a5ae40aabb576d1917740143a496f5a13c69ce4b
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2023-03-11 14:49:41 +0100

    Add `perl-image-exiftool` to dependencies

--- fetch_deps.sh
+++ fetch_deps.sh
@@ -5,16 +5,33 @@ set -e
 
 mkdir -p deps
 
+install_pacman() {
+    if [ -x "$(command -v pacman)" ]; then
+        if pacman -Qs "$1" > /dev/null; then
+            echo "Pacman: $1 is installed"
+        else
+            echo "Pacman: $1 is not installed: installing..."
+            sudo pacman -S "$1" --noconfirm
+        fi
+    else
+        echo "Pacman: command not fount, please install '$1' manually"
+    fi
+}
+
 fetch_repo() {
     if ! [ -d "deps/$2" ]; then
-        git clone "https://github.com/$1/$2.git" "deps/$2" &
+        git clone "https://github.com/$1/$2.git" "deps/$2"
     else
         cd "deps/$2"
-        git pull &
+        git pull
         cd ../..
     fi
 }
 
+# Pacman packages
+install_pacman perl-image-exiftool
+
+# tree-sitter
 fetch_repo nvim-treesitter nvim-treesitter
 
 fetch_repo tree-sitter tree-sitter-rust
@@ -29,6 +46,7 @@ fetch_repo rush-rs tree-sitter-hexdump
 fetch_repo wasm-lsp tree-sitter-wasm
 fetch_repo rush-rs tree-sitter-wasm-queries
 
+# rush dependencies
 fetch_repo rush-rs rush
 
 wait

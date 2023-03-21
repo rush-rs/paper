7da57db65306ed89577dcff3580656a1802ffa0c
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2023-03-21 19:26:43 +0100

    chapter 3: Implement all changes suggested by @RubixDev

--- listings/invoke_ld.sh
+++ listings/invoke_ld.sh
@@ -1,5 +1,6 @@
-ld -dynamic-linker /lib64/ld-linux-x86-64.so.2 \
-  /usr/lib/crt1.o \
-  /usr/lib/crti.o \
-  -lc input.o \
-  /usr/lib/crtn.o -o output
+ld -dynamic-linker /lib/ld-linux-x86-64.so.2 \
+    -lc /usr/lib/crt1.o \
+    -lc /usr/lib/crti.o \
+    -lc /usr/lib/crtn.o \
+    input.o \
+    -o output
